00b72c5c1545448a4583b6404c4e7b8692cabb17
brendadosanjos <brendakelly_oliveira@live.com>
2024-10-28 18:00:02 -0300

    parte inicial

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['media.licdn.com', 'i.pinimg.com', 'pbs.twimg.com'],
-  },
-}
-
-module.exports = nextConfig
---
+++ script.js
@@ -0,0 +1,21 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const feed = document.getElementById("feed");
+
+    // // Simulando uma API com dados de exemplo
+    // const characters = [
+    //     {
+    //         name: "Luke Skywalker",
+    //         description: "O icônico Jedi da saga Star Wars.",
+    //         image: "https://via.placeholder.com/300x200?text=Luke+Skywalker"
+    //     },
+    //     {
+    //         name: "Darth Vader",
+    //         description: "O vilão poderoso que se tornou um ícone.",
+    //         image: "https://via.placeholder.com/300x200?text=Darth+Vader"
+    //     },
+    //     {
+    //         name: "Leia Organa",
+    //         description: "A princesa da Rebelião com espírito forte.",
+    //         image: "https://via.placeholder.com/300x200?text=Leia+Organa"
+    //     }
+    // ];
