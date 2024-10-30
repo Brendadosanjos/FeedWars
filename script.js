ea8d7df9bbddf54f8259b85aba22a159ca1dc0a3
brendadosanjos <brendadosanjos.dev@gmail.com>
2024-10-30 16:55:40 -0300

    caixa de comentarios adicionada

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const feed = document.getElementById("feed");
+function users(){
+    fetch('data.json')
+     .then(reponse => reponse.json())
+     .then(users => {
+        console.log(users);
+     })
+}
 
-    // // Simulando uma API com dados de exemplo
-    // const characters = [
-    //     {
-    //         name: "Luke Skywalker",
-    //         description: "O icônico Jedi da saga Star Wars.",
-    //         image: "https://via.placeholder.com/300x200?text=Luke+Skywalker"
-    //     },
-    //     {
-    //         name: "Darth Vader",
-    //         description: "O vilão poderoso que se tornou um ícone.",
-    //         image: "https://via.placeholder.com/300x200?text=Darth+Vader"
-    //     },
-    //     {
-    //         name: "Leia Organa",
-    //         description: "A princesa da Rebelião com espírito forte.",
-    //         image: "https://via.placeholder.com/300x200?text=Leia+Organa"
-    //     }
-    // ];
+users()
